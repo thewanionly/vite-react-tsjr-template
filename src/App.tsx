6165f265e3e252f0a665d1501101ced43ffb6e3b
thewanionly <pelloani@gmail.com>
2023-04-27 04:11:21 +0800

    chore: install styled-components and polished

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import styled from 'styled-components'
+
+const S = {
+  App: styled.div``,
+  AppTitle: styled.h1`
+    font-weight: bold;
+  `,
+}
+
 export const App = () => {
-  return <h1>Vite + React + TSJR</h1>
+  return (
+    <S.App>
+      <S.AppTitle>Vite + React + TSJR</S.AppTitle>
+    </S.App>
+  )
 }
