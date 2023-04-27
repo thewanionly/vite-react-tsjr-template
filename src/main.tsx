f8a12c64b189c079190f14221ea4c52b8bfe83ba
thewanionly <pelloani@gmail.com>
2023-04-28 04:22:50 +0800

    refactor: move App under views folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { GlobalStyles, Theme } from 'styles'
-
-import { App } from './App.tsx'
+import { App } from 'views/App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
